8034f0cd7732d67c1ab3c4132bbeff531bf5e43b
Lord-Valen <lord_valen@protonmail.com>
2023-05-10 19:06:51 -0400

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A simple wayland native screenshot tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    std = {
+      url = "github:divnix/std";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, std, ...} @ inputs:
+    std.growOn {
+      inherit inputs;
+      cellsFrom = self + "/nix";
+      cellBlocks = with std.blockTypes; [
+        (installables "packages")
+      ];
+    }
+      {
+        packages = std.harvest self ["watershot" "packages"];
+      };
+}
