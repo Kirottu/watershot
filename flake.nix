cdd5cf1aaf85db8a16605608f7a6ee7c30dad794
Lord-Valen <46138807+Lord-Valen@users.noreply.github.com>
2023-05-12 01:08:20 -0400

    Flakify (#9)
    * chore: add flake
    
    * chore: add devshell
    
    * style: format flake.nix
    
    * docs: update README
    
    * chore: fix toml plugin
    
    * docs: fix commands spacing
    
    * chore: specify systems

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A simple wayland native screenshot tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    std = {
+      url = "github:divnix/std";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    std,
+    ...
+  } @ inputs:
+    std.growOn {
+      inherit inputs;
+      systems = ["x86_64-linux" "aarch64-linux"];
+      cellsFrom = self + "/nix";
+      cellBlocks = with std.blockTypes; [
+        (installables "packages")
+        (devshells "devshells")
+        (nixago "configs")
+      ];
+    }
+    {
+      packages = std.harvest self ["watershot" "packages"];
+      devShells = std.harvest self ["repo" "devshells"];
+    };
+}
